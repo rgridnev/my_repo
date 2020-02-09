cc85bcaf207f1eee6372cebc4e973535fc45bb4e
rgridnev <photo-gr@ya.ru>
2020-02-09 19:53:52 +0300

    vscode settings to gitignore

--- tlgbot/bot_idiot.py
+++ tlgbot/bot_idiot.py
@@ -1,18 +1,4 @@
-#from modules import dayscounter
-import datetime
-
-def dayscounter(year,mon,day, dest):
-    now = datetime.datetime.today()
-    NY = datetime.datetime(year,mon,day)
-    d = NY-now #  str(d)  '83 days, 2:43:10.517807'
-    mm, ss = divmod(d.seconds, 60)
-    hh, mm = divmod(mm, 60)
-    #result='1'
-    #result=str(print(dest, 'через {} дней {} часа {} мин {} сек.'.format(d.days, hh, mm, ss))
-    #result=print(dest, 'через {} дней {} часа {} мин {} сек.'.format(d.days, hh, mm, ss))
-    mmm = (dest, 'через {} дней {} часа {} мин {} сек.'.format(d.days, hh, mm, ss))
-    rrr=' '.join(mmm)
-    return rrr
+from modules import dayscounter
  
 # Подключаем модуль для Телеграма
 
@@ -30,7 +16,7 @@ from telebot import types
 
 # Настраиваем прокси
 from telebot import apihelper
-#apihelper.proxy = {'https': 'socks5h://geek:socks@t.geekclass.ru:7777'}
+apihelper.proxy = {'https': 'socks5h://geek:socks@t.geekclass.ru:7777'}
 #apihelper.proxy = {'https':'https://88.204.154.155:8080'}
 
  
